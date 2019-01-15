6ba785663c958fb5b334582a1cc979dd4c95f102
Malika <malika.baglan@gmail.com>
2019-01-15 15:52:48 -0600

    POC MasterPage

---
+++ MasterDetailPractice/MasterDetailPractice/MyMasterDetailPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MasterDetailPractice
+{
+    public partial class MyMasterDetailPage : MasterDetailPage
+    {
+        public MyMasterDetailPage()
+        {
+            InitializeComponent();
+            Detail = new NavigationPage(new MyPage1());
+            IsPresented = false;
+        }
+
+        void Handle_Clicked_1(object sender, System.EventArgs e)
+        {
+            Detail = new NavigationPage(new MyPage1());
+            IsPresented = false;
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            Detail = new NavigationPage(new MyPage2());
+            IsPresented = false;
+        }
+    }
+}
---
+++ MasterDetailPractice/MasterDetailPractice/MyPage1.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MasterDetailPractice
+{
+    public partial class MyPage1 : ContentPage
+    {
+        public MyPage1()
+        {
+            InitializeComponent();
+        }
+    }
+}
